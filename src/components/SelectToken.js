1934895c2aecee7c2c5749db3ae4cee4c1da47ea
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2019-10-19 10:48:32 -0400

    Update SelectToken.js
    update SOCKS -> URING

--- src/components/SelectToken.js
+++ src/components/SelectToken.js
@@ -33,7 +33,7 @@ const SelectMenu = styled(Select)`
 `
 
 const options = Object.keys(TOKEN_SYMBOLS)
-  .filter(s => s !== 'SOCKS')
+  .filter(s => s !== 'URING')
   .map(s => ({ value: s, label: s }))
 
 export default function SelectToken({ selectedTokenSymbol, setSelectedTokenSymbol }) {
